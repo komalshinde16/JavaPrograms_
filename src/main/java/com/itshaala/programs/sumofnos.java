5e2a6b12a80811bfd5a09c6ef073da594f0a7ff1
komalshinde16 <YOUR_GITHUB_EMAIL@gmail.com>
2026-01-29 19:03:07 +0530

    added

---
+++ src/main/java/com/itshaala/programs/sumofnos.java
@@ -0,0 +1,13 @@
+package com.itshaala.programs;
+
+public class sumofnos {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum =0;
+
+        for (int i =1; i<=5; i++){
+            sum = sum + i;
+        }
+        System.out.println("sum of the given no is : "  +sum);
+    }
+}
