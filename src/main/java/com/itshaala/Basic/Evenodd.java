a457c9d42dd5442a92ff35a61c378ea552d54451
komalshinde16 <YOUR_GITHUB_EMAIL@gmail.com>
2026-01-30 15:00:19 +0530

    added code of even & odd

---
+++ src/main/java/com/itshaala/Basic/Evenodd.java
@@ -0,0 +1,14 @@
+package com.itshaala.Basic;
+
+public class Evenodd {
+    public static void main(String[] args) {
+        int num = 9;
+
+        if(num % 2==0){
+            System.out.println("even number");
+
+        }else{
+            System.out.println("odd number");
+        }
+    }
+}
