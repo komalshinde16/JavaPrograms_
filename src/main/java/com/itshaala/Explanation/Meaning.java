c37d28ec6f16aed6c7b83cad0cbddb55800c7a58
komalshinde16 <komalambadasshinde@gmail.com>
2026-02-01 18:01:35 +0530

    Added meanings of Java number programs

---
+++ src/main/java/com/itshaala/Explanation/Meaning.java
@@ -0,0 +1,90 @@
+package com.itshaala.Explanation;
+
+
+public class Meaning {
+//SPY NUMBER***
+//
+//A spy number is a number in which
+//👉 sum of its digits = product of its digits
+//
+//Example:
+//        1124
+//
+//Sum = 1 + 1 + 2 + 4 = 8
+//
+//Product = 1 × 1 × 2 × 4 = 8
+//
+//        ✔ Since sum = product, 1124 is a spy number.
+
+
+//NEON NUMBER***
+
+//A neon number is a number where
+//👉 sum of digits of its square = the number itself
+//
+//Example:
+//        9
+//
+//Square = 9 × 9 = 81
+//
+//Sum of digits = 8 + 1 = 9
+//
+//        ✔ Since sum = number, 9 is a neon number.
+
+
+//Automorphic Number***
+//
+//An automorphic number is a number whose
+//👉 square ends with the same digits as the number itself
+//
+//Example:
+//        25
+//
+//Square = 25 × 25 = 625
+//
+//Ends with 25
+//
+//        ✔ 25 is an automorphic number
+
+
+//
+//🔹 Strong Number*****
+//
+//A strong number is a number where
+//👉 sum of factorial of its digits = the number
+//
+//Example:
+//        145
+//
+//        1! = 1
+//
+//        4! = 24
+//
+//        5! = 120
+//
+//Sum = 1 + 24 + 120 = 145
+//
+//        ✔ 145 is a strong number
+
+
+//ARMSTRONG NUMBER
+
+//An Armstrong number is a number where
+//👉 sum of cube of its digits = the number
+//        (for 3-digit numbers)
+//
+//Example:
+//        153
+//
+//        1³ = 1
+//
+//        5³ = 125
+//
+//        3³ = 27
+//
+//Sum = 153
+//
+//        ✔ 153 is an Armstrong number
+
+
+}
