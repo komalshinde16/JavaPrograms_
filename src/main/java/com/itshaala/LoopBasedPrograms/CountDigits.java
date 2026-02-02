d6b51471f252b64ee0b1cdd4f9f491e25b917557
komalshinde16 <komalambadasshinde@gmail.com>
2026-02-02 18:10:19 +0530

    added 2 programs

---
+++ src/main/java/com/itshaala/LoopBasedPrograms/CountDigits.java
@@ -0,0 +1,10 @@
+package com.itshaala.LoopBasedPrograms;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int num = 452145;
+        int count = String.valueOf(num).length();
+
+        System.out.println("The count of the digits in the num is :" + count);
+    }
+}
