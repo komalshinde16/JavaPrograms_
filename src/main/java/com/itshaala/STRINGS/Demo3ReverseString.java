d4d0f21c13249ac3424937beab699b7ed197edf3
komalshinde16 <komalambadasshinde@gmail.com>
2026-02-05 11:44:44 +0530

    reverse a string

---
+++ src/main/java/com/itshaala/STRINGS/Demo3ReverseString.java
@@ -0,0 +1,10 @@
+package com.itshaala.STRINGS;
+
+public class Demo3ReverseString {
+    public static void main(String[] args) {
+        String str = "REVERSE A STRING";
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        System.out.println(reversed);
+
+    }
+}
