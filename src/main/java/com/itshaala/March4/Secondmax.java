1f3520f4ecc3651fd01e4cbe6e799a3d1a50b024
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-04 19:11:57 +0530

    2ndmax

---
+++ src/main/java/com/itshaala/March4/Secondmax.java
@@ -0,0 +1,21 @@
+package com.itshaala.March4;
+
+public class Secondmax {
+    public static void main(String[] args) {
+        int[] array ={1,2,3};
+        int max = array[0];
+        int secondMax = array[1];
+
+        for(int i=0; i< array.length;i++){
+            if(array[i]>max){
+                secondMax = max;
+                max = array[i];
+            } else if (array[i]>secondMax && array[i]!=max) {
+                secondMax = array[i];
+
+            }
+        }
+        System.out.println(secondMax);
+    }
+
+}
