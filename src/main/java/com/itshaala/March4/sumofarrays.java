a6068d96fcb435218f0a03e4a29a2047879906b1
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-04 18:56:35 +0530

    sumofarrays

---
+++ src/main/java/com/itshaala/March4/sumofarrays.java
@@ -0,0 +1,12 @@
+package com.itshaala.March4;
+
+public class sumofarrays {
+    public static void main(String[] args) {
+        int[] array ={1,2,3};
+        int sum = 0;
+        for(int i=0; i< array.length;i++){
+            sum = sum + array[i];
+        }
+        System.out.println(sum);
+    }
+}
