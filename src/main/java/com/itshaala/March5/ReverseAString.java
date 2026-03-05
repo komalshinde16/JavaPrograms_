67bf28e0931f5a5f736a7899c9772048c11d523d
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-05 18:51:49 +0530

    rev

---
+++ src/main/java/com/itshaala/March5/ReverseAString.java
@@ -0,0 +1,11 @@
+package com.itshaala.March5;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        String str = "friend";
+        StringBuilder reversed = new StringBuilder(str).reverse();
+
+        System.out.println(reversed);
+
+    }
+}
