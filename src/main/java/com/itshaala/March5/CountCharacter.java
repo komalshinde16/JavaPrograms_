3d9a2226a852b4eda0cd65e8fbf52182524c0839
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-05 18:54:17 +0530

    count

---
+++ src/main/java/com/itshaala/March5/CountCharacter.java
@@ -0,0 +1,11 @@
+package com.itshaala.March5;
+
+public class CountCharacter {
+    public static void main(String[] args) {
+        String str = "KOMAL";
+        int count = str.length();
+
+        System.out.println(count);
+
+    }
+}
