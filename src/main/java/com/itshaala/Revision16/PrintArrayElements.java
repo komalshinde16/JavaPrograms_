325c32ee9f2e3851ded2459ed522569145dd2965
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-16 11:44:10 +0530

    printing

---
+++ src/main/java/com/itshaala/Revision16/PrintArrayElements.java
@@ -0,0 +1,14 @@
+package com.itshaala.Revision16;
+
+import java.util.Arrays;
+
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int[] array = {10,20,30,40,50,60,70,80,90};
+
+
+        System.out.println(Arrays.toString(array));
+
+    }
+
+}
