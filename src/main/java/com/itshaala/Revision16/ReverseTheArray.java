f62eccffb9074c062498e2ea1287c6380f407ac4
komalshinde16 <komalambadasshinde@gmail.com>
2026-03-16 12:56:23 +0530

    rev

---
+++ src/main/java/com/itshaala/Revision16/ReverseTheArray.java
@@ -0,0 +1,13 @@
+package com.itshaala.Revision16;
+
+public class ReverseTheArray {
+    public static void main(String[] args) {
+        int[] array = {10,20,30,40,50,60,70,80,90};
+
+        for(int i= array.length-1; i>=0; i--){
+            System.out.print(array[i]+ " ");
+        }
+
+    }
+
+}
